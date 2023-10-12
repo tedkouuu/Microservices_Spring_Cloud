61558d468126cbb6023007b96d62b1e4c440b2c9
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-10-12 20:58:20 +0300

    Create a simple hard coded currency exchange service

---
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,57 @@
+package com.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public CurrencyExchange() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public CurrencyExchange setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public CurrencyExchange setFrom(String from) {
+        this.from = from;
+        return this;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public CurrencyExchange setTo(String to) {
+        this.to = to;
+        return this;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public CurrencyExchange setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+        return this;
+    }
+}
